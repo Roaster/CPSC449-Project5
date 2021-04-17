0432ffc43fc2e4ac4651bc46d621c72d71b7b9e4
Andy-Lopez <79767788+Andy-Lopez@users.noreply.github.com>
2021-04-17 11:18:14 -0700

    Add query method to print all messages for user
    Also add timestamp in testmethod function

--- directmessage.py
+++ directmessage.py
@@ -1,6 +1,10 @@
 import boto3
 from bottle import route, run, post, get
 import uuid
+from datetime import datetime
+from botocore.exceptions import ClientError
+from pprint import pprint
+from boto3.dynamodb.conditions import Key, Attr
 
 #endpoint for sending a message from one user to another
 #def sendMessage()
@@ -8,7 +12,10 @@ import uuid
 def testmethod(fromUsername, toUsername):
     print(fromUsername + "\t" + toUsername)
     messageId = str(uuid.uuid4())
-    message = createMessage(messageId, fromUsername, "This is a test", toUsername, "sooneer",dynamodb=None)
+    #messageId = '1'
+    myDate = datetime.now()
+    myDate = str(myDate)
+    message = createMessage(messageId, fromUsername, "This is a test", toUsername, myDate, dynamodb=None)
 
 
 ###############################################################################
@@ -59,7 +66,10 @@ def create_movie_table(dynamodb=None):
                 'AttributeName': 'fromUser',
                 'AttributeType': 'S'
             },
-            
+                        {
+                'AttributeName': 'timestamp',
+                'AttributeType': 'S'
+            },
 
         ],
         ProvisionedThroughput={
@@ -69,6 +79,18 @@ def create_movie_table(dynamodb=None):
     )
     return table
 
+#gets all the messages from the given user. Uses PK fromUser. Prints them to console
+def getMessages(fromUser, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('DirectMessages')
+
+    response = table.query(
+        KeyConditionExpression=Key('fromUser').eq(fromUser)
+    )
+    items = response['Items']
+    print(items)
 
 #Method for deleting tables
 def delete_table(dynamodb=None):
@@ -86,8 +108,13 @@ def delete_table(dynamodb=None):
 if __name__ == '__main__':
     #create table
     #movie_table = create_movie_table()
-    #message = createMessage()
+    #message = testmethod('andy', 'test')
     #delete table
     #delete_table()
+    #to test this, create messages using testmethod above. It will print all messages created by given fromUser
+    #getMessages('andy')
+    #if getMessages:
+     #   print("Get message succeeded:")
+      #  pprint(getMessages, sort_dicts=False)
     run(host='localhost', port=8080, debug=True)
-    #print("Table status:", movie_table.table_status)
+    print("Table status:", movie_table.table_status)
